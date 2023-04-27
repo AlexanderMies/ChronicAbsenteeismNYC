68a0991629a1a01804e3f1b52f22f3fb260427e1
“Jamie <jz3037@columbia.edu>
2023-04-27 15:30:15 -0400

    edited choropleth & impact on test scores section

--- html5up-hyperspace/pages/embedNotebook.js
+++
@@ -1,4 +0,0 @@
-import { Runtime, Inspector } from "@observablehq/runtime";
-import notebook from "./b1fbb11dafedb0d3@96.js";
-
-new Runtime().module(notebook, Inspector.into(document.querySelector("#embedded-notebook")));
